3c00ae86cd5589f6008db0a97e7087643b6493f0
Fransua Boltof <bolt2000@ukr.net>
2023-05-27 22:38:38 +0300

    +lazy

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,25 @@
+import { lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import React from 'react';
-import Home from 'pages/Home';
-import SearchMovie from 'pages/SearchMovie';
-import MovieDetails from 'pages/MovieDetails';
-import Cast from 'components/Cast';
-import Reviews from 'components/Reviews';
+// import Home from 'pages/Home';
+// import SearchMovie from 'pages/SearchMovie';
+// import MovieDetails from 'pages/MovieDetails';
+// import Cast from 'components/Cast';
+// import Reviews from 'components/Reviews';
 import { Layout } from './Layout';
+
+
+
+const Home = lazy(() => import('../pages/Home'));
+const SearchMovie = lazy(() => import('../pages/SearchMovie'));
+const MovieDetails = lazy(() => import('../pages/MovieDetails'));
+// const Reviews = lazy(() => import('../components/Reviews'));
+
+
+const Cast = lazy(() => import('../components/Cast'));
+const Reviews = lazy(() => import('../components/Reviews'));
+
+
 
 export const App = () => {
   return (
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,10 @@
 import { useEffect, useState } from 'react';
 import { getMovies } from 'services/GetMoviesTrend';
-// import { useLocation } from 'react-router-dom';
-// import { Audio } from 'react-loader-spinner';
 import css from './Home.module.css';
 import MoviesList from 'components/MovieList';
 import Loading from 'components/Loading';
 
 const Home = () => {
-  // const location = useLocation();
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
@@ -16,7 +13,6 @@ const Home = () => {
 
     getMovies().then(movies => {
       setMovies(movies.results);
-      console.log(movies.results);
       setIsLoading(false);
     });
   }, []);
@@ -25,7 +21,6 @@ const Home = () => {
     <>
       {isLoading && <Loading />}
       <h1 className={css.home}>Trending today</h1>
-
       <MoviesList movies={movies} />
     </>
   );
